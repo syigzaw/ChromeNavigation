553dd10e5a95e7d1df2a24be5522e2ebd78a3139
GitSaad <saadsrahimi@gmail.com>
2016-06-25 20:58:10 -0400

    Adding Speech Recognition - needs microphone permission

---
+++ background.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+//window.alert("hello from background");
+//document.location.href = "http://stackoverflow.com/";
+//chrome.browserAction.onClicked.addListener(function (activeTab) {
+    //var newURL = "http://stackoverflow.com/";
+  //  chrome.tabs.create({ url: newURL });
+//});
