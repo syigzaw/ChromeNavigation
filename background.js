e6af47a88d89dfa3002e9f9cac05414b06743198
GitSaad <saadsrahimi@gmail.com>
2016-07-05 21:13:07 -0400

    Refactoring switching tabs by name

--- background.js
+++ background.js
@@ -1,17 +1,13 @@
 // JavaScript source code
-//window.alert("hello from background");
-//document.location.href = "http://stackoverflow.com/";
 //chrome.browserAction.onClicked.addListener(function (activeTab) {
     //var newURL = "http://stackoverflow.com/";
   //  chrome.tabs.create({ url: newURL });
 //});
 // JavaScript source code
-//window.alert('background running');
 /*chrome.tabs.create({'url': 'speechtotext.html', 'type': 'popup'}, function(window) {
 });*/
 
-/*window.alert('background running');
-
+/*
 chrome.onUpdated.addListener(function(tab) {
     // No tabs or host permissions needed!
     console.log('Turning ' + tab.url + ' red!');
@@ -59,23 +55,28 @@ function navigateChrome(text) {
         refresh();
     }
     else if(words.indexOf('switch')>=0){
-        chrome.tabs.getAllInWindow(null,function(tabs){
-            for(var i=0; i<tabs.length; i++){
-                var name = tabs[i].title.split(' ');
-                name = name[0];
-                if(name.toLowerCase() === words[1]){
-                    chrome.tabs.query({}, function (tabs) {
-                        chrome.tabs.update(tabs[i].id, { active: true });
-                    });
-                    break;
-                }
-            }
-        });
+        switchByTabName(words);
     }
 }
 
-function switchByTabName(){
-
+function switchByTabName(words){
+    chrome.tabs.getAllInWindow(null, function (tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+            var name = tabs[i].title.split(' ');
+            name = name[0];
+            if (name.toLowerCase().contains(words[1])) {
+                chrome.tabs.update(tabs[i].id, { active: true });
+                break;
+            }
+            // The above implementation could expand more easily
+            // to take care of more words to switch tab by
+            /*var title = tabs[i].title.toLowerCase();
+            if (title.indexOf(words[1]) == 0) {
+                chrome.tabs.update(tabs[i].id, { active: true });
+                break;
+            }*/
+        }
+    });
 }
 
 function exit(){
@@ -114,81 +115,80 @@ function up() {
     window.scrollBy(0,-500);
 }
 
-//thinking there is no need for this if statement
-if (true) {
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.onstart = function () {
-        recognizing = true;
-    };
-    recognition.onerror = function (event) {
-        if (event.error == 'no-speech') {
-            ignore_onend = true;
-        }
-        if (event.error == 'audio-capture') {
-            ignore_onend = true;
-        }
-        if (event.error == 'not-allowed') {
-            window.open("chrome-extension://" + chrome.runtime.id + "/errorCatching.html");
-            ignore_onend = true;
-        }
-    };
-    recognition.onend = function () {
-        recognizing = false;
-        if (ignore_onend) {
-            return;
-        }
-        if (!final_transcript) {
-            return;
-        }
-        navigateChrome(final_transcript);
-        final_transcript = '';
-    };
-    recognition.onresult = function (event) {
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-                final_transcript += event.results[i][0].transcript;
-                final_transcript = final_transcript.toLowerCase().trim();
-                var noSpaceWordArray = [];
-                var spaceWordArray = [];
-                var startAdding = false;
-                final_transcript.split(' ').forEach(function(i) {
-                    if (i.indexOf('.') >= 0) {
-                        startAdding = true;
+
+
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.onstart = function () {
+    recognizing = true;
+};
+recognition.onerror = function (event) {
+    if (event.error == 'no-speech') {
+        ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+        ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+        window.open("chrome-extension://" + chrome.runtime.id + "/errorCatching.html");
+        ignore_onend = true;
+    }
+};
+recognition.onend = function () {
+    recognizing = false;
+    if (ignore_onend) {
+        return;
+    }
+    if (!final_transcript) {
+        return;
+    }
+    navigateChrome(final_transcript);
+    final_transcript = '';
+};
+recognition.onresult = function (event) {
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+            final_transcript = final_transcript.toLowerCase().trim();
+            var noSpaceWordArray = [];
+            var spaceWordArray = [];
+            var startAdding = false;
+            final_transcript.split(' ').forEach(function(i) {
+                if (i.indexOf('.') >= 0) {
+                    startAdding = true;
+                }
+                if (startAdding) {
+                    if(i == 'slash') {
+                        i = '/';
                     }
-                    if (startAdding) {
-                        if(i == 'slash') {
-                            i = '/';
-                        }
-                        noSpaceWordArray.push(i);
-                    } else {
-                        spaceWordArray.push(i);
-                    }
-                });
-                final_transcript = spaceWordArray.join(' ');
-                final_transcript += ' ' + noSpaceWordArray.join('');
-                final_transcript = final_transcript.trim();
-                //if (run) {
-                    navigateChrome(final_transcript);
-               /* }
-                if (final_transcript == 'okay chrome' || final_transcript == 'ok chrome') {
-                    run = true;
-                    //alert("It's on!");
-                } else if (final_transcript == 'goodbye chrome') {
-                    run = false;
-                   // alert("It's off!");
-                }*/
-            }
-           /* else {
-                interim_transcript += event.results[i][0].transcript;
+                    noSpaceWordArray.push(i);
+                } else {
+                    spaceWordArray.push(i);
+                }
+            });
+            final_transcript = spaceWordArray.join(' ');
+            final_transcript += ' ' + noSpaceWordArray.join('');
+            final_transcript = final_transcript.trim();
+            //if (run) {
+                navigateChrome(final_transcript);
+           /* }
+            if (final_transcript == 'okay chrome' || final_transcript == 'ok chrome') {
+                run = true;
+                //alert("It's on!");
+            } else if (final_transcript == 'goodbye chrome') {
+                run = false;
+               // alert("It's off!");
             }*/
         }
-        interim_transcript = interim_transcript.toLowerCase().trim();
-        console.log(1, final_transcript, 2, interim_transcript);
-        final_transcript = '';
-    };
-}
+       /* else {
+            interim_transcript += event.results[i][0].transcript;
+        }*/
+    }
+    interim_transcript = interim_transcript.toLowerCase().trim();
+    console.log(1, final_transcript, 2, interim_transcript);
+    final_transcript = '';
+};
 
 if (recognizing) {
     recognition.stop();
